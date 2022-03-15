c8efb41919bddfc249c47a578325bfbc367f087d
Asmith18 <Smittysadam@gmail.com>
2022-03-15 14:18:47 -0500

    changed TVC to VC becasue they are UIView Controllers not UITableView Controllers.

---
+++ One Trick Companion/Controllers/Content/Maps/MapsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MapsViewController.swift
+//  One Trick Companion
+//
+//  Created by adam smith on 3/15/22.
+//
+
+import UIKit
+
+class MapsViewController: UIViewController {
+    
+//MARK: - Outlets
+    @IBOutlet weak var mapsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+}
---
+++ One Trick Companion/Controllers/Leaderboard/LeaderboardViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LeaderboardViewController.swift
+//  One Trick Companion
+//
+//  Created by adam smith on 3/15/22.
+//
+
+import UIKit
+
+class LeaderboardViewController: UIViewController {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var leaderboardTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+}
