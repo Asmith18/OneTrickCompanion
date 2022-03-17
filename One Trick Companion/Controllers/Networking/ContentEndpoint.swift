d348db48d214a9d63a3ce51af8dadb438fab5b7e
Asmith18 <Smittysadam@gmail.com>
2022-03-17 14:25:31 -0500

    updated apikey

--- One Trick Companion/Controllers/Networking/ContentEndpoint.swift
+++ One Trick Companion/Controllers/Networking/ContentEndpoint.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 
 extension URL {
-    static let leaderboardBaseURL = URL(string: "https://na.api.riotgames.com/val/ranked/v1/leaderboards/by-act/52e9749a-429b-7060-99fe-4595426a0cf7?size=200&startIndex=0&api_key=RGAPI-aac06334-4b05-40c0-999a-55b27af72e43")
+    static let leaderboardBaseURL = URL(string: "https://na.api.riotgames.com/val/ranked/v1/leaderboards/by-act/52e9749a-429b-7060-99fe-4595426a0cf7?size=200&startIndex=0&api_key=RGAPI-8f7cd289-790f-4da5-abfd-a860cc62cfe5")
     static let mapsBaseURL = URL(string: "https://valorant-api.com/v1/maps")
     
 }
