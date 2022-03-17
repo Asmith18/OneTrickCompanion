4d4259f12fc9d5dcd39510f097b37d5c16cc955a
Asmith18 <Smittysadam@gmail.com>
2022-03-17 16:18:34 -0500

    finished writing the files now time to debugg

--- One Trick Companion/Controllers/Content/Weapons/WeaponsTableViewCell.swift
+++ One Trick Companion/Controllers/Content/Weapons/WeaponsTableViewCell.swift
@@ -11,11 +11,23 @@ class WeaponsTableViewCell: UITableViewCell {
 
 //MARK: - Outlets
     @IBOutlet weak var gunNameTextLabel: UILabel!
-    @IBOutlet weak var gunImageView: UIImageView!
+    @IBOutlet weak var gunImageView: MapImageView!
     @IBOutlet weak var fireRateTextLabel: UILabel!
     @IBOutlet weak var magSizeTextLabel: UILabel!
     @IBOutlet weak var fbaTextLabel: UILabel!
     @IBOutlet weak var stat1TextLabel: UILabel!
     @IBOutlet weak var stat2TextLabel: UILabel!
     @IBOutlet weak var stat3TextLabel: UILabel!
+    
+    var viewModel: WeaponsViewModel!
+    
+    func updateViews(weapon: WeaponsData) {
+        gunNameTextLabel.text = weapon.displayName
+        fetchImage(with: weapon.displayIcon ?? "none")
+    }
+    
+    func fetchImage(with urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        gunImageView.setImage(using: url)
+    }
 }
