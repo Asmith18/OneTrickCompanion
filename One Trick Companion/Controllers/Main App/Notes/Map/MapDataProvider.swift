fe9364c12400c235b7c0d5ac3e38370256049ba3
Asmith18 <Smittysadam@gmail.com>
2022-03-22 19:41:50 -0500

    finished writing the maps files for the new flow

---
+++ One Trick Companion/Controllers/Main App/Notes/Map/MapDataProvider.swift
@@ -0,0 +1,8 @@
+//
+//  MapDataProvider.swift
+//  One Trick Companion
+//
+//  Created by adam smith on 3/22/22.
+//
+
+import Foundation
---
+++ One Trick Companion/Controllers/Main App/Notes/Map/MapViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MapViewModel.swift
+//  One Trick Companion
+//
+//  Created by adam smith on 3/22/22.
+//
+
+import Foundation
