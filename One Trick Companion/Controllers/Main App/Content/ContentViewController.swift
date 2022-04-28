504c266d89f9c4dcd04bd97ba51441ec31bd78d1
Asmith18 <Smittysadam@gmail.com>
2022-04-28 16:49:34 -0500

    bug fixes

--- One Trick Companion/Controllers/Main App/Content/ContentViewController.swift
+++ One Trick Companion/Controllers/Main App/Content/ContentViewController.swift
@@ -20,7 +20,7 @@ class ContentViewController: UIViewController {
         super.viewDidLoad()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
     }
     
