735f7a18b9f6043e8cc0bd0a5839b84444d1fd3d
Adam Smith <Smittysadam@gmail.com>
2023-07-21 15:37:26 -0500

    content tab is not a collection view.

---
+++ Lineup Larry/Main App/Content/ContentCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ContentCollectionViewCell.swift
+//  Lineup Larry
+//
+//  Created by Adam Smith on 7/21/23.
+//
+
+import UIKit
+
+class ContentCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var cellLabel: UILabel!
+}
--- Lineup Larry/Main App/Content/ContentViewController.swift
+++ Lineup Larry/Main App/Content/ContentViewController.swift
@@ -8,23 +8,18 @@
 import UIKit
 import SafariServices
 
-class ContentViewController: UIViewController {
-
-    @IBOutlet weak var agentView: UIView!
-    @IBOutlet weak var mapView: UIView!
-    @IBOutlet weak var weaponView: UIView!
-    @IBOutlet weak var tosView: UIView!
+class ContentViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
+    @IBOutlet weak var contentCollectionView: UICollectionView!
+    
+    let spacing: CGFloat = 10
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupView()
-    }
-    
-    func setupView() {
-        agentView.layer.cornerRadius = 12
-        mapView.layer.cornerRadius = 12
-        weaponView.layer.cornerRadius = 12
-        tosView.layer.cornerRadius = 12
+        title = "Contents"
+        contentCollectionView.delegate = self
+        contentCollectionView.dataSource = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,13 +28,78 @@ class ContentViewController: UIViewController {
         navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
         navigationController?.navigationBar.barTintColor = UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 0)
     }
-//MARK: - Actions
-
     
-    @IBAction func termsAndConditionsButtonPressed(_ sender: Any) {
-        guard let url = URL(string: "https://www.termsfeed.com/live/8f3ca374-a82c-4d9e-a487-c5c35fd6e49a") else { return }
-                let viewController = SFSafariViewController(url: url)
-        present(viewController, animated: true)
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "content", for: indexPath) as? ContentCollectionViewCell else { return  UICollectionViewCell() }
+        
+        switch indexPath.item {
+        case 0:
+            cell.cellLabel.text = "Agents"
+            cell.cellView.layer.cornerRadius = 25
+        case 1:
+            cell.cellLabel.text = "Maps"
+            cell.cellView.layer.cornerRadius = 25
+        case 2:
+            cell.cellLabel.text = "Weapons"
+            cell.cellView.layer.cornerRadius = 25
+        case 3:
+            cell.cellLabel.text = "TOS"
+            cell.cellView.layer.cornerRadius = 25
+        default:
+            cell.cellLabel.text = nil
+            cell.cellView.layer.cornerRadius = 25
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        switch indexPath.item {
+        case 0:
+            let storyboard = UIStoryboard(name: "AgentContent", bundle: nil)
+            guard let viewController = storyboard.instantiateViewController(withIdentifier: "AgentContent") as? AgentsViewController else { return }
+            self.navigationController?.pushViewController(viewController, animated: false)
+        case 1:
+            let storyboard = UIStoryboard(name: "MapContent", bundle: nil)
+            guard let viewController = storyboard.instantiateViewController(withIdentifier: "MapContent") as? MapsViewController else { return }
+            self.navigationController?.pushViewController(viewController, animated: false)
+        case 2:
+            let storyboard = UIStoryboard(name: "WeaponContent", bundle: nil)
+            guard let viewController = storyboard.instantiateViewController(withIdentifier: "WeaponContent") as? WeaponsViewController else { return }
+            self.navigationController?.pushViewController(viewController, animated: false)
+        case 3:
+            guard let url = URL(string: "https://www.termsfeed.com/live/8f3ca374-a82c-4d9e-a487-c5c35fd6e49a") else { return }
+            let viewController = SFSafariViewController(url: url)
+            present(viewController, animated: true)
+        default:
+            let storyboard = UIStoryboard(name: "Content", bundle: nil)
+            guard let viewController = storyboard.instantiateViewController(withIdentifier: "content") as? ContentViewController else { return }
+            self.navigationController?.pushViewController(viewController, animated: false)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // Calculate the cell size with 2 cells per row and equal spacing between cells and edges
+        let spacing: CGFloat = 10 // You can adjust the spacing here as needed
+        let width = (collectionView.frame.width - 3 * spacing) / 2
+        let height = width // 1:1 aspect ratio
+        
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return spacing
     }
 }
 
